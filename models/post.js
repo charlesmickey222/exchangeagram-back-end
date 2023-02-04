aa762476eee09b9d16bbea7c4d4a7eb6ac75d9ec
Drew Neece <dneece5@aggienetwork.com>
2023-02-04 11:45:35 -0600

    add embedded comments  model

---
+++ models/post.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  },
+  { timestamps: true }
+)
+
+const postSchema = new Schema(
+  {
+    photo: {
+      type: String,
+      required: true
+    },
+    caption: String,
+    comments: [commentSchema],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  },
+  { timestamps: true }
+)
+
+const Post = mongoose.model('Post', postSchema)
+
+export {
+  Post
+}
