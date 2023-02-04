221f866534a493a75df9462b1946be60164f4a63
Drew Neece <dneece5@aggienetwork.com>
2023-02-04 12:00:14 -0600

    add embedded likes  model

---
+++ models/post.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  },
+  { timestamps: true }
+)
+
+const likeSchema = new Schema(
+  {
+    like: Boolean,
+    dislike: Boolean,
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  },
+  { timestamps: true }
+)
+
+const postSchema = new Schema(
+  {
+    photo: {
+      type: String,
+      required: true
+    },
+    caption: String,
+    comments: [commentSchema],
+    likes: [likeSchema],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  },
+  { timestamps: true }
+)
+
+const Post = mongoose.model('Post', postSchema)
+
+export {
+  Post
+}
