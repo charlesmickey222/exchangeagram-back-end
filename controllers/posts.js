e49bc83c26021b3ec7864bb7af5c3749426bcc7c
Drew Neece <dneece5@aggienetwork.com>
2023-02-04 14:12:41 -0600

    add .js to imports

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,5 @@
-import { Profile } from "../models/profile";
-import { Post } from "../models/post";
+import { Profile } from "../models/profile.js"
+import { Post } from "../models/post.js";
 
 async function index(req, res) {
   try {
