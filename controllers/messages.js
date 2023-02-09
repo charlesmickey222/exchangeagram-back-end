97acc236e46eb80e65f60fe0ff2ddd416bf5ac88
justinpastine <justin.pastine@gmail.com>
2023-02-09 18:56:43 -0500

    clean up back end code

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,13 +2,11 @@ import { Message } from '../models/message.js'
 import { Profile } from '../models/profile.js'
 
 async function createMessage(req, res) {
-  console.log('TEST')
   try {
     req.body.author = req.user.profile
     const message = await Message.create(req.body)
     res.status(201).json(message)
   } catch (error) {
-    console.log(error)
     res.status(500).json(error)
   }
 }
@@ -19,7 +17,6 @@ async function messageIndex(req, res) {
     .populate('author')
     res.status(200).json(messages)
   } catch (error) {
-    console.log(error)
     res.status(500).json(error)
   }
 }
